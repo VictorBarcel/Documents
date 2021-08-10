9dd9b9add2ce6d28014d6b821a0ef44e814ca6e6
victorbarcelos0 <87201245+victorbarcelos01@users.noreply.github.com>
2021-08-10 14:55:37 -0300

    Create EAP - Mapa mental.mm

---
+++ EAP - Mapa mental.mm
@@ -0,0 +1,151 @@
+<map version="freeplane 1.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Gerenciamento de Projeto" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1627597475256" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" STYLE="bubble" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Primeira Etapa" POSITION="right" ID="ID_68506732" CREATED="1627595933267" MODIFIED="1627597052266" COLOR="#660066">
+<edge COLOR="#ff0000"/>
+<font BOLD="true"/>
+<node ID="ID_1726690307" CREATED="1627596049701" MODIFIED="1627597523963" COLOR="#00cc33"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#009900">Dominar ferramentas</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<font BOLD="false"/>
+<node TEXT="Scilab" ID="ID_190302284" CREATED="1627596230387" MODIFIED="1627597256211" COLOR="#3333ff"/>
+<node TEXT="Xcos" ID="ID_16248000" CREATED="1627596232918" MODIFIED="1627597260665" COLOR="#3333ff"/>
+</node>
+<node TEXT="Realizar estudos de implementação" ID="ID_785792757" CREATED="1627596142478" MODIFIED="1627597427454" COLOR="#00cc33">
+<node TEXT="Quais protocolos de comunicação utilizar" ID="ID_528434287" CREATED="1627596293600" MODIFIED="1627597175740" COLOR="#ff6600"/>
+</node>
+<node TEXT="Estudar a viabilidade do projeto em tempo determinado" ID="ID_1406451747" CREATED="1627596143180" MODIFIED="1627597511529" COLOR="#00cc33"/>
+<node TEXT="Avaliação" ID="ID_1003226645" CREATED="1627596143477" MODIFIED="1627597516246" COLOR="#00cc33">
+<node TEXT="Com os conhecimentos adquiridos, julgar se é suficiente para seguir para a próxima etapa." ID="ID_392866170" CREATED="1627596323968" MODIFIED="1627597417581" COLOR="#ff6600">
+<font BOLD="false"/>
+</node>
+</node>
+</node>
+<node TEXT="Segunda Etapa" POSITION="right" ID="ID_903425146" CREATED="1627595937251" MODIFIED="1627597064341" COLOR="#660066">
+<edge COLOR="#0000ff"/>
+<font BOLD="true"/>
+<node TEXT="Divisão do trabalho para os demais integrantes do grupo." ID="ID_1023227650" CREATED="1627596429294" MODIFIED="1627597276446" COLOR="#00cc33">
+<node TEXT="Pesquisa" ID="ID_748148537" CREATED="1627596558812" MODIFIED="1627597185391" COLOR="#ff6600"/>
+<node TEXT="Implementação" ID="ID_893937758" CREATED="1627596564436" MODIFIED="1627597189640" COLOR="#ff6600"/>
+</node>
+<node TEXT="Estudar as estratégias de implementação" ID="ID_1004315958" CREATED="1627596438347" MODIFIED="1627597280742" COLOR="#00cc33"/>
+<node TEXT="Entender a implementação dos protocolos" ID="ID_865089465" CREATED="1627596438660" MODIFIED="1627597285944" COLOR="#00cc33">
+<node TEXT="Modbus" ID="ID_911134825" CREATED="1627596575418" MODIFIED="1627597198701" COLOR="#ff6600"/>
+<node TEXT="Hart" ID="ID_791454" CREATED="1627596577636" MODIFIED="1627597203717" COLOR="#ff6600"/>
+</node>
+<node TEXT="Desenvolver gateways virtuais para o protocolo escolhido" ID="ID_1972352824" CREATED="1627596438941" MODIFIED="1627597290177" COLOR="#00cc33"/>
+<node TEXT="Simulação" ID="ID_1904816612" CREATED="1627596439160" MODIFIED="1627597298901" COLOR="#00cc33">
+<node TEXT="Simulação concluída" ID="ID_560657616" CREATED="1627596594864" MODIFIED="1627597220983" COLOR="#ff6600">
+<node TEXT="Próxima fase do projeto" ID="ID_739747922" CREATED="1627596625913" MODIFIED="1627597246926" COLOR="#3333ff"/>
+</node>
+<node TEXT="Simulação não concluída" ID="ID_188804273" CREATED="1627596596239" MODIFIED="1627597226915" COLOR="#ff6600">
+<node TEXT="Analisar a segunda etapa" ID="ID_515168570" CREATED="1627596640550" MODIFIED="1627597241162" COLOR="#3333ff">
+<font BOLD="false"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Terceira Etapa" POSITION="right" ID="ID_596380052" CREATED="1627595939141" MODIFIED="1627597080057" COLOR="#660066">
+<edge COLOR="#00ff00"/>
+<font BOLD="true"/>
+<node TEXT="Estudar ferramentas utilizadas na indústria que poderiam ser implementadas no projeto" ID="ID_257671086" CREATED="1627596425030" MODIFIED="1627597306540" COLOR="#00cc33"/>
+<node TEXT="Desenvolvimento do ambiente de simulação para processos e instrumentos industriais no Scilab que interaja com essas ferramentas" ID="ID_841256731" CREATED="1627596666605" MODIFIED="1627597310539" COLOR="#00cc33"/>
+<node TEXT="Analisar se o projeto está proporcionando aos usuários, mais conhecimento sobre as ferramentas e instrumentos utilizados por engenheiros da indústria" ID="ID_596020478" CREATED="1627596666949" MODIFIED="1627597315475" COLOR="#00cc33"/>
+<node TEXT="Simulação" ID="ID_613298480" CREATED="1627596667183" MODIFIED="1627597322368" COLOR="#00cc33">
+<node TEXT="Simulação concluída" ID="ID_167350836" CREATED="1627596897310" MODIFIED="1627597367627" COLOR="#ff6600">
+<node TEXT="Finalização do projeto" ID="ID_263468123" CREATED="1627596923574" MODIFIED="1627597398839" COLOR="#3333ff"/>
+</node>
+<node TEXT="Simulação não concluída" ID="ID_1369178374" CREATED="1627596902545" MODIFIED="1627597373284" COLOR="#ff6600">
+<node TEXT="Análise da segunda etapa" ID="ID_44741376" CREATED="1627596947913" MODIFIED="1627597404526" COLOR="#3333ff"/>
+</node>
+</node>
+</node>
+<node TEXT="Quarta Etapa" POSITION="right" ID="ID_1782849347" CREATED="1627595960929" MODIFIED="1627597090532" COLOR="#660066">
+<edge COLOR="#00ffff"/>
+<font BOLD="true"/>
+<node TEXT="Analisar se é possível melhorar o projeto a longo prazo e resaltar tais ideias" ID="ID_581339095" CREATED="1627596975873" MODIFIED="1627597382346" COLOR="#00cc33">
+<font BOLD="false"/>
+</node>
+<node TEXT="Entrega do projeto" ID="ID_1605213243" CREATED="1627596979716" MODIFIED="1627597387860" COLOR="#00cc33"/>
+</node>
+</node>
+</map>
